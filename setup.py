08f472699e164a4c40311e88a26c7b2b94ab36d6
Nimboss2411 <74679516+Nimboss2411@users.noreply.github.com>
2021-07-16 10:15:37 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme: readme = readme.read()
+
+setup(
+    name = 'owopy',
+    version = '0.0.1',
+    description = 'An easy to use module to OwOify your text - turn it into furry Japanese Baby Babblespeak!',
+    long_description = readme,
+    url = 'https://github.com/Nimboss2411/OwOpy',
+    author = 'Nimit Grover',
+    author_email = 'nimbossthegreat@gmail.com',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Other Audience',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9'
+    ],
+    keywords = 'owo owoify',
+    packages = ['OwOpy'],
+    install_requires = []
+)
